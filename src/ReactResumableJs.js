3c7b56a8fa15a49b1f4e8dcd0a35b642c233b22b
widi <widi.aji@java-valley.com>
2019-03-08 14:33:25 +0700

    ulalawalawala

--- src/ReactResumableJs.js
+++ src/ReactResumableJs.js
@@ -8,7 +8,7 @@
  */
 
 import React from "react";
-import Resumablejs from "resumablejs";
+import Resumablejs from "./resumablejs";
 
 export default class ReactResumableJs extends React.Component {
     constructor(props) {
